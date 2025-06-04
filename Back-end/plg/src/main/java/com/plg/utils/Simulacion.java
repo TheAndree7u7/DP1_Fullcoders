a591a8e91b59ef708f42dc703f504115b85011f2
AndreeCSOR <a20203495@pucp.edu.pe>
2025-06-03 23:23:26 -0500

    ahroa se quedo

--- Back-end/plg/src/main/java/com/plg/utils/Simulacion.java
+++ Back-end/plg/src/main/java/com/plg/utils/Simulacion.java
@@ -109,9 +109,7 @@ public class Simulacion {
 
         System.out.println("\n=== RESUMEN FINAL DE LA SIMULACIÓN ===");
         System.out.println("📈 Métricas de rendimiento:");
-        System.out.println("   • Tiempo de inicio: " + tiempoInicial);
-        System.out.println("   • Tiempo de finalización: " + tiempoFinal);
-        System.out.println("   • Duración total: " + tiempoEjecucion.getSeconds() + " segundos");
+ 
         System.out.println("   • Kilómetros totales recorridos: " + Parametros.kilometrosRecorridos);
         System.out.println("   • Fitness global alcanzado: " + Parametros.fitnessGlobal);
         System.out.println("\n=== FIN DE LA SIMULACIÓN ===\n");
