077d92aad7de604d144b41eb10d2b09545f0a74d
AndreeCSOR <a20203495@pucp.edu.pe>
2025-06-22 16:07:50 -0500

    ahora esta mejorado el request

--- Back-end/plg/src/main/java/com/plg/dto/request/AveriaRequest.java
+++ Back-end/plg/src/main/java/com/plg/dto/request/AveriaRequest.java
@@ -1,17 +1,34 @@
 package com.plg.dto.request;
 
-import lombok.Data;
 import java.time.LocalDateTime;
+
+import com.plg.entity.Coordenada;
+import com.plg.entity.TipoIncidente;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 /**
  * DTO para la creación de una nueva avería.
  */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Data
 public class AveriaRequest {
 
     private String codigoCamion;
-    private String turno; // T1, T2, T3
-    private String tipoIncidente; // TI1, TI2, TI3
+    private TipoIncidente tipoIncidente; // TI1, TI2, TI3
     private LocalDateTime fechaInicio;
     private LocalDateTime fechaFin;
+    private Coordenada coordenada;
+    private LocalDateTime fechaHoraReparacion;
+    private LocalDateTime fechaHoraDisponible;
+    private int turnoOcurrencia;
+    private double tiempoReparacionEstimado;
+    private Boolean estado; // true: activo, false: inactivo
 }
