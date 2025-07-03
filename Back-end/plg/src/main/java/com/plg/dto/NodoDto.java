fe6d0664470d4af8f772541203536a480a8c8065
AndreeCSOR <a20203495@pucp.edu.pe>
2025-07-03 18:23:22 -0500

    nodo

--- Back-end/plg/src/main/java/com/plg/dto/NodoDto.java
+++ Back-end/plg/src/main/java/com/plg/dto/NodoDto.java
@@ -1,14 +1,20 @@
 package com.plg.dto;
 
 import com.plg.entity.Nodo;
+import com.plg.entity.TipoNodo;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
+@Getter
+@Setter
 public class NodoDto {
     private CoordenadaDto coordenada;
-
+    private TipoNodo tipo;
     public NodoDto(Nodo nodo) {
         this.coordenada = new CoordenadaDto(nodo.getCoordenada());
+        this.tipo = nodo.getTipoNodo();
     }
 }
