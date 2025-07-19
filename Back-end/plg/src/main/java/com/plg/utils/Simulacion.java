0cf79717e99fd217d7172b06e182fbe19fba1083
AndreeCSOR <a20203495@pucp.edu.pe>
2025-07-19 17:16:55 -0500

    ahora tien la data de camiones

--- Back-end/plg/src/main/java/com/plg/utils/Simulacion.java
+++ Back-end/plg/src/main/java/com/plg/utils/Simulacion.java
@@ -64,7 +64,7 @@ public class Simulacion {
         // 1. Obtenemos todos los pedidos del fechaActual < x < fechaActual +
         // intervaloTiempo
         LocalDateTime fecha_inferior = Parametros.fecha_inicial.minusMinutes(Parametros.intervaloTiempo);
-        LocalDateTime fecha_superior = Parametros.fecha_inicial.plusMinutes(1);
+        LocalDateTime fecha_superior = Parametros.fecha_inicial.plusMinutes(10);
         List<Pedido> pedidosEnRango = Parametros.dataLoader.pedidos.stream()
                 .filter(pedido -> pedido.getFechaRegistro().isAfter(fecha_inferior)
                         && pedido.getFechaRegistro().isBefore(fecha_superior))
