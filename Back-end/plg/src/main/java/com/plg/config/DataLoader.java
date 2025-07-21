16684f2d29f7d359f3fbe4f71b2b0fe396c6a75e
JhairtVegaQ <116320255+JhairtVegaQ@users.noreply.github.com>
2025-07-20 20:58:35 -0500

    ALGORITMO OPTIMIZADO AL 100 %
    Refactored the genetic algorithm and truck assignment logic in Individuo.java to improve modularity and clarity. Added new methods for route planning, truck and warehouse selection, and fitness calculation. Commented out the creation of certain truck types in DataLoader.java and set the genetic algorithm population size to 1 in AlgoritmoGenetico.java. Introduced new utility methods in Gen.java for distance calculation and retrieving the last node in a route.

--- Back-end/plg/src/main/java/com/plg/config/DataLoader.java
+++ Back-end/plg/src/main/java/com/plg/config/DataLoader.java
@@ -68,18 +68,18 @@ public class DataLoader {
             Camion camion = CamionFactory.crearCamionesPorTipo(TipoCamion.TA, true, coordenadaCentral);
             this.camiones.add(camion);
         }
-        for (int i = 0; i < 4; i++) {
-            Camion camion = CamionFactory.crearCamionesPorTipo(TipoCamion.TB, true, coordenadaCentral);
-            this.camiones.add(camion);
-        }
-        for (int i = 0; i < 4; i++) {
-            Camion camion = CamionFactory.crearCamionesPorTipo(TipoCamion.TC, true, coordenadaCentral);
-            this.camiones.add(camion);
-        }
-        for (int i = 0; i < 10; i++) {
-            Camion camion = CamionFactory.crearCamionesPorTipo(TipoCamion.TD, true, coordenadaCentral);
-            this.camiones.add(camion);
-        }
+        // for (int i = 0; i < 4; i++) {
+        //     Camion camion = CamionFactory.crearCamionesPorTipo(TipoCamion.TB, true, coordenadaCentral);
+        //     this.camiones.add(camion);
+        // }
+        // for (int i = 0; i < 4; i++) {
+        //     Camion camion = CamionFactory.crearCamionesPorTipo(TipoCamion.TC, true, coordenadaCentral);
+        //     this.camiones.add(camion);
+        // }
+        // for (int i = 0; i < 10; i++) {
+        //     Camion camion = CamionFactory.crearCamionesPorTipo(TipoCamion.TD, true, coordenadaCentral);
+        //     this.camiones.add(camion);
+        // }
         return this.camiones;
     }
 
--- Back-end/plg/src/main/java/com/plg/utils/AlgoritmoGenetico.java
+++ Back-end/plg/src/main/java/com/plg/utils/AlgoritmoGenetico.java
@@ -28,7 +28,7 @@ public class AlgoritmoGenetico {
 
     public AlgoritmoGenetico(Mapa mapa) {
         this.mapa = mapa;
-        poblacionTamano = 20;
+        poblacionTamano = 1;
     }
 
     public void ejecutarAlgoritmo() {
