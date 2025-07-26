a1391a70e973eb9ea8122140f48b763252a8f389
gerardo <gerardo@gerardos-MacBook-Air-2.local>
2025-07-26 00:38:27 -0500

    metrica de flota funcional sin incluir camiones averiados

--- Front-end/src/views/SimulacionAlColapso.tsx
+++ Front-end/src/views/SimulacionAlColapso.tsx
@@ -9,6 +9,7 @@ import { useSimulacion } from "../context/SimulacionContext";
 import { formatearTiempoTranscurrido } from "../context/simulacion/utils/tiempo";
 import { useCurrentDateTime } from "../hooks/useCurrentDateTime";
 import IndicadorGLPTotal from "../components/IndicadorGLPTotal";
+import IndicadorFlotaFuncional from "../components/IndicadorFlotaFuncional";
  
 
 const SimulacionSemanal: React.FC = () => {
@@ -122,6 +123,9 @@ const SimulacionSemanal: React.FC = () => {
             <div className="text-sm flex items-center gap-4">
               <div>
                 <IndicadorGLPTotal />
+              </div>
+              <div>
+                <IndicadorFlotaFuncional />
               </div>
               <div>
                 <span className="mr-2">Fecha y hora de la simulacion:</span>
